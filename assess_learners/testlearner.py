27434a8f5150a3c61f60763e9e19929a70416cea
pranavrao1 <pranavrao1@gmail.com>
2018-09-24 10:49:46 -0700

    final changes to plot

--- assess_learners/testlearner.py
+++ assess_learners/testlearner.py
@@ -30,71 +30,124 @@ import RTLearner as rt
 import BagLearner as bl
 import InsaneLearner as il
 import sys
-
+import util
+import matplotlib.pyplot as plt
 
 def run_learner(learner, trainX, trainY, testX, testY):
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     print learner.author()
     learner.addEvidence(trainX, trainY)  # training step
     # # evaluate in sample
     predY = learner.query(trainX)  # get the predictions
-    rmse = math.sqrt(((trainY - predY) ** 2).sum() / trainY.shape[0])
+    rmse_in = math.sqrt(((trainY - predY) ** 2).sum() / trainY.shape[0])
     print
     print "In sample results"
-    print "RMSE: ", rmse
+    print "RMSE: ", rmse_in
     c = np.corrcoef(predY, y=trainY)
     print "corr: ", c[0, 1]
 
     # evaluate out of sample
     predY = learner.query(testX)  # get the predictions
-    rmse = math.sqrt(((testY - predY) ** 2).sum() / testY.shape[0])
+    rmse_out = math.sqrt(((testY - predY) ** 2).sum() / testY.shape[0])
     print
     print "Out of sample results"
-    print "RMSE: ", rmse
+    print "RMSE: ", rmse_out
     c = np.corrcoef(predY, y=testY)
     print "corr: ", c[0, 1]
+    print "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    return rmse_in, rmse_out
 
 
-if __name__=="__main__":
-    if len(sys.argv) != 2:  		   	  			    		  		  		    	 		 		   		 		  
-        print "Usage: python testlearner.py <filename>"  		   	  			    		  		  		    	 		 		   		 		  
-        sys.exit(1)  		   	  			    		  		  		    	 		 		   		 		  
-    inf = open(sys.argv[1])  		   	  			    		  		  		    	 		 		   		 		  
-    data = np.array([map(float,s.strip().split(',')) for s in inf.readlines()])
-    print "Istanbul"
-    datafile = 'Istanbul.csv'
+def generate_plots(datafile = 'Istanbul.csv'):
+    np.random.seed(903205913)
+    print " Plotting graphs for " + str(datafile)
     with util.get_learner_data_file(datafile) as f:
         alldata = np.genfromtxt(f, delimiter=',')
         # Skip the date column and header row if we're working on Istanbul data
         if datafile == 'Istanbul.csv':
             alldata = alldata[1:, 1:]
-    data = alldata[1:, 1:]
+    data = alldata
     # print data.shape[0]
     data = np.random.permutation(data)
-
     # compute how much of the data is training and testing
     np.random.shuffle(data)
-    train_rows = int(0.6* data.shape[0])  		   	  			    		  		  		    	 		 		   		 		  
-    test_rows = data.shape[0] - train_rows  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    # separate out training and testing data  		   	  			    		  		  		    	 		 		   		 		  
-    trainX = data[:train_rows,0:-1]  		   	  			    		  		  		    	 		 		   		 		  
-    trainY = data[:train_rows,-1]  		   	  			    		  		  		    	 		 		   		 		  
-    testX = data[train_rows:,0:-1]  		   	  			    		  		  		    	 		 		   		 		  
-    testY = data[train_rows:,-1]
-  		   	  			    		  		  		    	 		 		   		 		  
-    # create a learner and train it  		   	  			    		  		  		    	 		 		   		 		  
-    #learner = lrl.LinRegLearner(verbose = True) # create a LinRegLearner
-    #run_learner(learner, trainX, trainY, testX, testY) # training step
-    #
-    #learner = dt.DTLearner(leaf_size=1, verbose=False)  # constructor
-    #run_learner(learner, trainX, trainY, testX, testY)  # training step
-    #
-    # learner = rt.RTLearner(leaf_size=1, verbose=False)  # constructor
-    # run_learner(learner, trainX, trainY, testX, testY)
+    train_rows = int(0.6 * data.shape[0])
+    test_rows = data.shape[0] - train_rows
+    # separate out training and testing data
+    trainX = data[:train_rows, 0:-1]
+    trainY = data[:train_rows, -1]
+    testX = data[train_rows:, 0:-1]
+    testY = data[train_rows:, -1]
+    rmses_in = np.empty(100)
+    rmses_in.fill(-1)
+    rmses_out = np.empty(100)
+    rmses_out.fill(-1)
+    leaf_size = range(100)
+    for i in leaf_size:
+        learner = dt.DTLearner(leaf_size=i, verbose=False)  # constructor
+        rmses_in[i], rmses_out[i] = run_learner(learner, trainX, trainY, testX, testY)  # training step
+    plt.plot(leaf_size, rmses_in, label="In Sample")
+    plt.plot(leaf_size, rmses_out, label="Out Sample")
+    plt.legend(loc="best")
+    plt.grid(True)
+    plt.title('DTLearner overfitting with increasing leaf size.', fontsize=10)
+    plt.ylabel('RMSE')
+    plt.xlabel('Leaf Size')
+    plt.savefig('DTLearner overfitting with increasing leaf size.')
+    plt.clf()
+    plt.cla()
+    plt.close()
+    for i in leaf_size:
+        kwargs = {'leaf_size': i}
+        learner = bl.BagLearner(learner=dt.DTLearner, kwargs=kwargs, verbose=False, bags=20)  # constructor
+        rmses_in[i], rmses_out[i] = run_learner(learner, trainX, trainY, testX, testY)  # training step
+    plt.plot(leaf_size, rmses_in, label="In Sample")
+    plt.plot(leaf_size, rmses_out, label="Out Sample")
+    plt.legend(loc="best")
+    plt.grid(True)
+    plt.title('Baglearner at 20 bags overfitting with increasing leaf size.', fontsize=10)
+    plt.ylabel('RMSE')
+    plt.xlabel('Leaf Size')
+    plt.savefig('Baglearner at 20 bags overfitting with increasing leaf size.')
+    plt.clf()
+    plt.cla()
+    plt.close()
 
-    # learner = bl.BagLearner(learner=lrl.LinRegLearner, kwargs={}, bags=10, boost=False, verbose=False)
-    # run_learner(learner, trainX, trainY, testX, testY)
 
+def run_debuging_tests():
+    inf = open(sys.argv[1])
+    data = np.array([map(float, s.strip().split(',')) for s in inf.readlines()])
+    # print data.shape[0]
+    data = np.random.permutation(data)
+    # compute how much of the data is training and testing
+    np.random.shuffle(data)
+    train_rows = int(0.6 * data.shape[0])
+    test_rows = data.shape[0] - train_rows
+    # separate out training and testing data
+    trainX = data[:train_rows, 0:-1]
+    trainY = data[:train_rows, -1]
+    testX = data[train_rows:, 0:-1]
+    testY = data[train_rows:, -1]
+    # create a learner and train it
+    learner = lrl.LinRegLearner(verbose=True)  # create a LinRegLearner
+    run_learner(learner, trainX, trainY, testX, testY)  # training step
+    learner = dt.DTLearner(leaf_size=1, verbose=False)  # constructor
+    run_learner(learner, trainX, trainY, testX, testY)  # training step
+    learner = rt.RTLearner(leaf_size=1, verbose=False)  # constructor
+    run_learner(learner, trainX, trainY, testX, testY)
+    learner = bl.BagLearner(learner=lrl.LinRegLearner, kwargs={}, bags=10, boost=False, verbose=False)
+    run_learner(learner, trainX, trainY, testX, testY)
     learner = il.InsaneLearner(verbose=False)
     run_learner(learner, trainX, trainY, testX, testY)
+
+
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print "Usage: python testlearner.py <filename>"
+        generate_plots()
+        sys.exit(1)
+    generate_plots(sys.argv[1])
+
+    #TODO: This was use by the student to run additional developmental steps. Uncomment if you would like to run it.
+    # run_debuging_tests()
 

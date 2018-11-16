38a9748fea9e6c635d73682023db520159c3bcf1
pranavrao1 <pranavrao1@gmail.com>
2018-11-15 20:59:07 -0800

    Cleaning Manual Strategy changes

---
+++ manual_strategy/submitted/TheoreticallyOptimalStrategy.py
@@ -0,0 +1,137 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
+from util import get_data, plot_data
+import marketsimcode as ms
+import matplotlib.dates as matplot_dates
+import matplotlib.pyplot as plt
+
+def author():
+    return 'prao43'
+
+def testPolicy(symbol = "AAPL",
+               sd=dt.datetime(2010,1,1),
+               ed=dt.datetime(2011,12,31),
+               sv=100000):
+    dates = pd.date_range(sd, ed)
+    prices_df = get_data([symbol], dates)
+    prices_df.fillna(method='ffill', inplace=True)
+    prices_df.fillna(method='bfill', inplace=True)
+    prices_normalized_df = prices_df / prices_df.ix[0, :]
+
+    shares_df = pd.DataFrame(0, index = prices_normalized_df.index, columns = ['Shares'])
+    orders_df = pd.DataFrame('HOLD', index = prices_normalized_df.index, columns = ['Order'])
+    symbols_df = pd.DataFrame(symbol, index = prices_normalized_df.index, columns = ['Symbol'])
+    holdings_df = pd.DataFrame(0, index = prices_normalized_df.index, columns = ['Holdings'])
+
+    number_of_entries = prices_normalized_df.shape[0]
+    total_holdings = 0
+    for index in range(number_of_entries-1):
+        current_date = prices_normalized_df.index[index]
+        new_date = prices_normalized_df.index[index + 1]
+        current_price = prices_normalized_df.loc[current_date][symbol]
+        new_price = prices_normalized_df.loc[new_date][symbol]
+        if (new_price > current_price) and (total_holdings < 1000):
+            orders_df.loc[current_date]['Order'] = 'BUY'
+            if total_holdings == 0:
+                shares_df.loc[current_date]['Shares'] = 1000
+                holdings_df.loc[current_date]['Holdings'] = 1000
+                total_holdings += 1000
+            else:
+                shares_df.loc[current_date]['Shares'] = 2000
+                holdings_df.loc[current_date]['Holdings'] = 2000
+                total_holdings += 2000
+        elif (new_price < current_price) and (total_holdings > -1000):
+            orders_df.loc[current_date]['Order'] = 'SELL'
+            if total_holdings == 0:
+                shares_df.loc[current_date]['Shares'] = 1000
+                holdings_df.loc[current_date]['Holdings'] = -1000
+                total_holdings -= 1000
+            else:
+                shares_df.loc[current_date]['Shares'] = 2000
+                holdings_df.loc[current_date]['Holdings'] = -2000
+                total_holdings -= 2000
+    trades_df = pd.concat([symbols_df, orders_df, shares_df, holdings_df], axis=1)
+    trades_df.columns = ['Symbol', 'Order', 'Shares', 'Holdings']
+    return trades_df
+
+def testPolicyBenchmark(symbol = "AAPL",
+               sd=dt.datetime(2010,1,1),
+               ed=dt.datetime(2011,12,31),
+               sv=100000):
+    dates = pd.date_range(sd, ed)
+    prices_df = get_data([symbol], dates)
+    prices_df.fillna(method='ffill', inplace=True)
+    prices_df.fillna(method='bfill', inplace=True)
+    prices_normalized_df = prices_df / prices_df.ix[0, :]
+
+    shares_df = pd.DataFrame(0, index = prices_normalized_df.index, columns = ['Shares'])
+    orders_df = pd.DataFrame('HOLD', index = prices_normalized_df.index, columns = ['Order'])
+    symbols_df = pd.DataFrame(symbol, index = prices_normalized_df.index, columns = ['Symbol'])
+    holdings_df = pd.DataFrame(0, index = prices_normalized_df.index, columns = ['Holdings'])
+
+    current_date = prices_normalized_df.index[0]
+    shares_df.loc[current_date]['Shares'] = 1000
+    trades_df = pd.concat([symbols_df, orders_df, shares_df, holdings_df], axis=1)
+    trades_df.columns = ['Symbol', 'Order', 'Shares', 'Holdings']
+    return trades_df
+
+if __name__ == "__main__":
+    pd.set_option('chained_assignment', None)
+    start_date = dt.datetime(2008,1,1)
+    end_date = dt.datetime(2009,12,31)
+    start_value = 100000
+
+    trades_df = testPolicy(symbol="JPM", sd=start_date, ed=end_date, sv=start_value)
+    portfolio_value = ms.compute_portvals(trades_df, start_val=start_value, commission=0.00, impact=0.0)
+    portfolio_value.fillna(method='ffill', inplace=True)
+    portfolio_value.fillna(method='bfill', inplace=True)
+    portfolio_value_norm = portfolio_value / portfolio_value.ix[0, :]
+
+    trades_df_benchmark = testPolicyBenchmark(symbol="JPM", sd=start_date, ed=end_date, sv=start_value)
+    benchmark_value = ms.compute_portvals(trades_df_benchmark, start_val=start_value, commission=0.00, impact=0.0)
+    benchmark_value.fillna(method='ffill', inplace=True)
+    benchmark_value.fillna(method='bfill', inplace=True)
+    benchmark_value_norm = benchmark_value / benchmark_value.ix[0, :]
+
+    first_plot = portfolio_value_norm.plot(grid=True, title='Best Possible Strategy', use_index=True, color='black')
+    first_plot.xaxis.set_major_locator(matplot_dates.MonthLocator())
+    first_plot.xaxis.set_major_formatter(matplot_dates.DateFormatter('%b'))
+    first_plot.xaxis.set_minor_locator(matplot_dates.YearLocator())
+    first_plot.xaxis.set_minor_formatter(matplot_dates.DateFormatter('%Y'))
+    first_plot.xaxis.set_tick_params(which='minor', pad=20)
+    date_min = dt.date(trades_df.index.min().year, 1, 1)
+    date_max = dt.date(trades_df.index.max().year + 1, 1, 1)
+    first_plot.set_xlabel("Date")
+    first_plot.set_ylabel("Normalized Stock Price")
+    first_plot.set_xlim(date_min, date_max)
+
+    second_plot = benchmark_value_norm.plot(grid=True, title='Best Possible Strategy', use_index=True, color='blue')
+    second_plot.xaxis.set_major_locator(matplot_dates.MonthLocator())
+    second_plot.xaxis.set_major_formatter(matplot_dates.DateFormatter('%b'))
+    second_plot.xaxis.set_minor_locator(matplot_dates.YearLocator())
+    second_plot.xaxis.set_minor_formatter(matplot_dates.DateFormatter('%Y'))
+    second_plot.xaxis.set_tick_params(which='minor', pad=20)
+    second_plot.set_xlabel("Date")
+    second_plot.set_ylabel("Normalized Stock Price")
+    second_plot.set_xlim(date_min, date_max)
+    plt.legend(('Portfolio Value', 'Benchmark'), loc='best', prop={'size': 12})
+    plt.savefig('Best Possible Strategy.png')
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+    print "NORMALIZED IN SAMPLE"
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = ms.compute_portfolio_stats(portfolio_value_norm)
+    cum_ret_bench, avg_daily_ret_bench, std_daily_ret_bench, sharpe_ratio_bench = ms.compute_portfolio_stats(benchmark_value_norm)
+    print "Sharpe Ratio of Portfolio: " + str(sharpe_ratio)
+    print "Sharpe Ratio of Benchmark : " + str(sharpe_ratio_bench)
+    print
+    print "Cumulative Return of Portfolio: " + str(cum_ret)
+    print "Cumulative Return of Benchmark: " + str(cum_ret_bench)
+    print
+    print "Standard Deviation of Portfolio: " + str(std_daily_ret)
+    print "Standard Deviation of Benchmark: " + str(std_daily_ret_bench)
+    print
+    print "Average Daily Return of Portfolio: " + str(avg_daily_ret)
+    print "Average Daily Return of Benchmark: " + str(avg_daily_ret_bench)
---
+++ manual_strategy/submitted/indicators.py
@@ -0,0 +1,105 @@
+import datetime as dt
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as matplot_dates
+import matplotlib.ticker as mticker
+from matplotlib import gridspec
+import numpy as np
+import os
+import pandas as pd
+
+from util import get_data, plot_data
+
+def author():
+    return 'prao43'
+
+def set_plot_values(first_plot):
+    first_plot.xaxis.set_major_locator(months)
+    first_plot.xaxis.set_major_formatter(monthsFmt)
+    first_plot.xaxis.set_minor_locator(years)
+    first_plot.xaxis.set_minor_formatter(yearsFmt)
+    first_plot.xaxis.set_tick_params(which='minor', pad=20)
+    sma_date_min = dt.date(sma_df.index.min().year, 1, 1)
+    sma_date_max = dt.date(sma_df.index.max().year + 1, 1, 1)
+    first_plot.set_xlabel("Date")
+    first_plot.set_ylabel("Normalized Stock Price")
+    first_plot.set_xlim(sma_date_min, sma_date_max)
+
+
+if __name__ == "__main__":
+    stock_ticker = ['JPM']
+    start_date = dt.datetime(2008,1,1)
+    end_date = dt.datetime(2009,12,31)
+    dates = pd.date_range(start_date, end_date)
+    df = get_data(stock_ticker, dates)
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+    normalized_df = df / df.ix[0, :]
+    years = matplot_dates.YearLocator()  # every year
+    months = matplot_dates.MonthLocator()  # every month
+    yearsFmt = matplot_dates.DateFormatter('%Y')
+    monthsFmt = matplot_dates.DateFormatter('%b')
+
+    ## GET SMA
+    columns_sma = ['SMA', 'Price/SMA']
+    sma_df = pd.DataFrame(0, index = normalized_df.index, columns = columns_sma)
+    sma_df['SMA'] = normalized_df[stock_ticker[0]].rolling(window=20, center=False).mean()
+    sma_df['Price/SMA'] = normalized_df[stock_ticker[0]] / sma_df['SMA']
+    sma_plot = pd.concat([normalized_df[stock_ticker[0]], sma_df['SMA'], sma_df['Price/SMA']], axis=1)
+    sma_plot.columns = [stock_ticker[0], 'SMA', 'Price/SMA']
+    first_plot = sma_plot.plot(grid=True, title='Simple Moving Average (SMA)', use_index=True)
+    set_plot_values(first_plot)
+    plt.legend((stock_ticker[0], 'SMA', 'Price/SMA', '1.05 norm line', '0.95 norm line'), loc='best', prop={'size': 8})
+    plt.savefig('Prices per Simple Moving Average.png')
+
+    ## Get BOLLINGER bands
+    columns_bb = ['Lower', 'Upper', 'Deviation']
+    bollinger_bands_df = pd.DataFrame(0, index=normalized_df.index, columns=columns_bb)
+    bollinger_bands_df['Deviation'] = normalized_df.rolling(window=20).std()
+    bollinger_bands_df['Upper'] = sma_df['SMA'] + (2 * bollinger_bands_df['Deviation'])
+    bollinger_bands_df['Lower'] = sma_df['SMA'] - (2 * bollinger_bands_df['Deviation'])
+    bb_plot = pd.concat([normalized_df[stock_ticker[0]], bollinger_bands_df['Lower'], bollinger_bands_df['Upper']], axis=1)
+    bb_plot.columns = [stock_ticker[0], 'Lower BB', 'Upper BB']
+    second_plot = bb_plot.plot(grid=True, title='Bollinger Bands', use_index=True)
+    x_axis = bollinger_bands_df.index.get_level_values(0)
+    second_plot.fill_between(x_axis, bollinger_bands_df['Upper'], bollinger_bands_df['Lower'], color='gray', label='Bollinger Bands', alpha=0.2)
+    set_plot_values(second_plot)
+    plt.savefig('Bollinger Bands for Stock.png')
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+    ## GET SMA PERCENT
+    bollinger_bands_percent_df = pd.DataFrame(0, index=normalized_df.index, columns=['Percent'])
+    bollinger_bands_percent_df['Percent'] = (normalized_df[stock_ticker[0]] - bollinger_bands_df['Lower']) / (bollinger_bands_df['Upper'] - bollinger_bands_df['Lower'])
+    third_plot = bollinger_bands_percent_df.plot(grid=True, title='Bollinger Bands Percentage', use_index=True)
+    third_plot.legend()
+    third_plot.xaxis.set_major_locator(months)
+    third_plot.xaxis.set_major_formatter(monthsFmt)
+    third_plot.xaxis.set_minor_locator(years)
+    third_plot.xaxis.set_minor_formatter(yearsFmt)
+    third_plot.xaxis.set_tick_params(which='minor', pad=20)
+    percent_date_min = dt.date(bollinger_bands_percent_df.index.min().year, 1, 1)
+    percent_date_max = dt.date(bollinger_bands_percent_df.index.max().year + 1, 1, 1)
+    third_plot.set_xlabel("Date")
+    third_plot.set_ylabel("BB Percentage")
+    third_plot.set_xlim(percent_date_min, percent_date_max)
+    plt.axhline(y=1.00, color='black', linestyle='--')
+    plt.axhline(y=0.00, color='black', linestyle='--')
+    plt.axhspan(1.00, 0.00, alpha=0.3, color='gray')
+    plt.savefig('Bollinger Bands Percent for Stock.png')
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+    ### Get Momentum
+    momentum_df = pd.DataFrame(0, index=normalized_df.index, columns=['Momentum'])
+    momentum_df['Momentum'] = normalized_df[stock_ticker[0]].diff(20) / normalized_df[stock_ticker[0]].shift(20)
+    momentum_plot = pd.concat([normalized_df[stock_ticker[0]], momentum_df], axis=1)
+    fourth_plot = momentum_plot.plot(grid=True, title='Momentum', use_index=True)
+    set_plot_values(fourth_plot)
+    plt.legend(('JPM', 'Momentum'), loc='best', prop={'size': 8})
+    plt.savefig('Stock Momentum and Price.png', tight=True)
+    plt.clf()
+    plt.cla()
+    plt.close()
